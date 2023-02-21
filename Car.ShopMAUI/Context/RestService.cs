8704d35ff4d31a96654ad2f7f279eb1706a7ca70
Yael Chavez <you400@gmail.com>
2023-02-21 17:45:51 -0600

    rest service

---
+++ Car.ShopMAUI/Context/RestService.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Text.Json;
+
+namespace Car.ShopMAUI.Context
+{
+    public class RestService
+    {
+        HttpClient _httpClient;
+        Uri _urlBase;
+
+        public RestService()
+        {
+            _urlBase = new Uri("https://dwshopwebclient20230216212752.azurewebsites.net/api");
+            _httpClient = new();
+            _httpClient.BaseAddress = _urlBase;
+        }
+
+        public async Task<List<Models.Car>> GetCars()
+        {
+            var response = await _httpClient.GetAsync("/CarsForSalesApi");
+           
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+
+
+                return JsonSerializer.Deserialize<List<Models.Car>>(content,
+                    new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            }
+
+            throw new Exception("Error al tratar de obtener la informacion");
+        
+        }
+
+    }
+}
