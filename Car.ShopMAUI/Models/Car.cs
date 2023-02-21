d6d345f9134c7fe23800bba87e53119c2eb6a223
Yael Chavez <you400@gmail.com>
2023-02-20 20:57:47 -0600

    cars for sale

---
+++ Car.ShopMAUI/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace Car.ShopMAUI.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int Year { get; set; }
+        public string PhotoUrl { get; set; }
+
+        public double? Lat { get; set; }
+        public double? Lon { get; set; }
+    }
+}
--- Car.ShopMAUI/Views/CarsForSale.xaml.cs
+++ Car.ShopMAUI/Views/CarsForSale.xaml.cs
@@ -2,8 +2,40 @@ namespace Car.ShopMAUI.Views;
 
 public partial class CarsForSale : ContentPage
 {
-	public CarsForSale()
-	{
-		InitializeComponent();
-	}
+    private List<Models.Car> cars;
+    public CarsForSale()
+    {
+        InitializeComponent();
+        cars = new()
+        {
+           new()
+            {
+               Brand="Chevrolet",
+               Model="Camaro",
+               Description="Camaro SS 2023 500km",
+               Price = 100_000,
+               Year=2023,
+               PhotoUrl="https://acroadtrip.blob.core.windows.net/catalogo-imagenes/s/RT_V_fdf2792f4ef94f45a2624c08ef7ebcca.jpg"
+            },
+             new()
+            {
+               Brand="Chevrolet",
+               Model="Camaro",
+               Description="Camaro SS 2023 500km",
+               Price = 100_000,
+               Year=2023,
+               PhotoUrl="https://acroadtrip.blob.core.windows.net/catalogo-imagenes/s/RT_V_fdf2792f4ef94f45a2624c08ef7ebcca.jpg"
+            },
+             new()
+            {
+               Brand="Chevrolet",
+               Model="Camaro",
+               Description="Camaro SS 2023 500km",
+               Price = 100_000,
+               Year=2023,
+               PhotoUrl="https://acroadtrip.blob.core.windows.net/catalogo-imagenes/s/RT_V_fdf2792f4ef94f45a2624c08ef7ebcca.jpg"
+            }
+        };
+        CarsList.ItemsSource = cars;
+    }
 }
