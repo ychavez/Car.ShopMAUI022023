a67b4969319cfbc1d9c49503ddc41847804c44d4
Yael Chavez <you400@gmail.com>
2023-02-20 18:19:29 -0600

    Agregar archivos de proyecto.

---
+++ Car.ShopMAUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Car.ShopMAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Car.ShopMAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Car.ShopMAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
